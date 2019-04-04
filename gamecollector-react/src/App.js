3045e31ecadfa0bb0455f546deaafa46601e963a
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-04-04 13:08:21 -0400

    started axios

--- gamecollector-react/src/App.js
+++ gamecollector-react/src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
 import Header from "./Components/Header";
 import NavBar from "./Components/NavBar";
+import axios from "axios";
 
 import "./App.css";
 
 class App extends Component {
+  state = {
+    consoles: []
+  };
+
+  //retrieve console and game date for first page
+  componentDidMount() {
+    axios.get("/consoles").then(res => this.setState({ consoles: res.data }));
+  }
+  //add company
+
+  //add game console
+
+  //add game
+
+  //delete game
+
   render() {
     return (
       <div className="app">
